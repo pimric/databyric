f59ff05f8eeca42832c275557ef69962c92cab7f
Ric Lamalice <riclamalice@mbp-de-ric.home>
2025-05-14 16:31:35 +0200

    Lets goov4

--- src/app/services/conception/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-    return (
-      <div className="p-12 text-center">
-        Page “Conception” – contenu à venir
-      </div>
-    );
-  }
-  
--- src/app/services/page.tsx
+++
@@ -1,7 +0,0 @@
-// src/app/services/page.tsx
-import Services from "@/components/Services";
-
-
-export default function ServicesPage() {
-  return <Services />;
-}
